4221c0a958cca9c91abceb0920df313ffb0095e6
Vcp289 <meesompop@gmail.com>
2026-08-02 15:44:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.innerHTML = `
 
 <button id="btnCalc">Calc</button>
 <button id="btnClear">Clear</button>
-<button onclick="findL()">L</button>
+<button onclick="findL()">LL</button>
 
 <table id="t"></table>
 
