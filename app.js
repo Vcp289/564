8326cb146f5fc979aa954245538e3656ceb92ed6
Vcp289 <meesompop@gmail.com>
2026-08-02 10:05:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,32 @@
-const app=document.getElementById('app');
-app.innerHTML=`<div>
-<input maxlength=1 id=a>
-<input maxlength=1 id=b>
-<input maxlength=1 id=c>
-<input maxlength=1 id=d>
-<input maxlength=1 id=e>
-<button onclick='calc()'>Calc</button>
-<table id=t></table>
-</div>`;
+const app = document.getElementById("app");
 
-function w(n){
-    return (n+10)%10;
-}
+app.innerHTML = `
+<div>
+    <input maxlength="1" id="a">
+    <input maxlength="1" id="b">
+    <input maxlength="1" id="c">
+    <input maxlength="1" id="d">
+    <input maxlength="1" id="e">
 
-function calc(){
-    let A=+a.value,
-        B=+b.value,
-        C=+c.value,
-        D=+d.value,
-        E=+e.value;
+    <button onclick="calc()">Calc</button>
 
-    let g=[
-        [A,w(A-1),w(A+1),D,w(D+1)],
-        [B,w(B-1),w(B+1),E,w(E-1)],
-        [C,w(C-1),w(C+1),w(D-1),w(E+1)]
-    ];
+    <table id="t"></table>
+</div>
+`;
 
-    t.innerHTML=g.map(r=>'<tr>'+r.map(x=>'<td>'+x+'</td>').join('')+'</tr>').join('');
-}
-
-const inputs [...document.querySelectorAll("input[maxlength='1']")];
+const inputs = [...document.querySelectorAll("input[maxlength='1']")];
 
 inputs.forEach((input,index)=>{
 
     input.addEventListener("input",(e)=>{
+
         e.target.value=e.target.value.replace(/[^0-9]/g,"");
 
         if(e.target.value.length===1 && index<inputs.length-1){
             inputs[index+1].focus();
             inputs[index+1].select();
         }
+
     });
 
     input.addEventListener("keydown",(e)=>{
