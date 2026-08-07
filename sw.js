cf1dfba42ee486a9baf8ec6c7469adf7ab0c95fe
Vcp289 <meesompop@gmail.com>
2026-08-07 15:23:23 +0700

    V6.4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "lucky-number-v6-4-1-ios-polish-20260807-6410";
-const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
+const CACHE = "lucky-number-v6-3-visual-polish-20260807-6300";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch", e => {
