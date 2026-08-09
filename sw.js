df41f79eb90e695569e4d71f62e874113128f2b1
Vcp289 <meesompop@gmail.com>
2026-08-09 07:55:02 +0700

    V

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lucky-number-v6-9-2-modal-safe-close-20260809";
+const CACHE = "lucky-number-v6-9-3-365-home-20260809";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png", "./icons/favicon-32.png"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
