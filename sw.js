a62b9f68994ad072a3d921a28f7c365b1de4fe4e
Vcp289 <meesompop@gmail.com>
2026-08-10 08:33:33 +0700

    V

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lucky-number-v6-9-5-history-incremental-save-20260810a";
+const CACHE = "lucky-number-v6-9-7-profile-winner-detail-20260809";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png", "./icons/favicon-32.png"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
@@ -6,5 +6,3 @@ self.addEventListener("fetch", e => {
   if (e.request.method !== "GET") return;
   e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
 });
-
-// 7-draw winner build
