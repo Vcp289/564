bc2c3fa838e158165272620c9ab743c33ce249ba
Vcp289 <meesompop@gmail.com>
2026-08-16 19:45:01 +0700

    V

--- sw-r18.js
+++ sw-r18.js
@@ -1,5 +1,5 @@
-const CACHE = "lucky-number-v6-10-40-r54-startup-fastpath-20260816a";
-const ASSETS = ["./", "./index.html", "./style-r18.css?v=61040r54startup-fastpath", "./app-r18.js?v=61040r54startup-fastpath", "./manifest.json?v=61040r54startup-fastpath", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png", "./icons/favicon-32.png"];
+const CACHE = "lucky-number-v6-10-40-r55-instant-first-paint-20260816a";
+const ASSETS = ["./", "./index.html", "./style-r18.css?v=61040r55instant-first-paint", "./app-r18.js?v=61040r55instant-first-paint", "./manifest.json?v=61040r55instant-first-paint", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png", "./icons/favicon-32.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
