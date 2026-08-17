13f2450236e86f77923bd27f7c8d85e804bb4dfc
Vcp289 <meesompop@gmail.com>
2026-08-17 08:42:24 +0700

    V

---
+++ sw-r20.js
@@ -0,0 +1,31 @@
+const CACHE = "lucky-number-v7-09-today-top5-trusted-20260817";
+const ASSETS = ["./", "./index.html", "./style-r19.css?v=709todaytop5", "./app-r20.js?v=709todaytop5", "./manifest.json?v=709todaytop5", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png", "./icons/favicon-32.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put("./index.html", copy)).catch(() => {});
+      return response;
+    }).catch(() => caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (response && response.ok) {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    }
+    return response;
+  })));
+});
