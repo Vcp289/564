d4069f6d8b44cfa74f8808341360a5aa84922852
Vcp289 <meesompop@gmail.com>
2026-08-19 18:39:19 +0700

    V2

--- sw-r32.js
+++ sw-r32.js
@@ -1,11 +1,11 @@
-const CACHE = "lucky-number-v7-10-00-ai-meta-all-pages-20260819";
+const CACHE = "lucky-number-v7-09-72-rank-movement-delta-20260819";
 const CACHE_PREFIX = "lucky-number-";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style-r32.css?v=71000aimeta",
-  "./app-r32.js?v=71000aimeta",
-  "./manifest.json?v=71000aimeta",
+  "./style-r32.css?v=70972rankdelta",
+  "./app-r32.js?v=70972rankdelta",
+  "./manifest.json?v=70972rankdelta",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/apple-touch-icon.png",
