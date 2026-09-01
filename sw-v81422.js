1671d1380b6798877d0d800428a0fbc6ad1a4163
Vcp289 <meesompop@gmail.com>
2026-09-01 13:26:49 +0700

    V

--- sw-v81422.js
+++ sw-v81422.js
@@ -1,4 +1,4 @@
-const BUILD = "81423settingsprofilefix1";
+const BUILD = "81422settingsprofilefix1";
 const CACHE_PREFIX = "lucky-number-shell-";
 const CACHE = `${CACHE_PREFIX}${BUILD}`;
 const RELEASE = `./releases/${BUILD}/`;
