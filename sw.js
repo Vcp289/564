2a0df8d6807885ff751fd2bc30ee87cebf6e6c56
Vcp289 <meesompop@gmail.com>
2026-09-04 08:47:35 +0700

    Accelerate Turbo rebuild startup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD = "81600x4native2";
+const BUILD = "81600x4native3";
 const CACHE_PREFIX = "lucky-number-shell-";
 const CACHE = `${CACHE_PREFIX}${BUILD}`;
 // The cache build changes on every deploy. Assets remain in the existing release
