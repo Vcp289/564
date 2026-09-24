f79fa74052ccd82ddc2c9904bad185c444cbf8c4
Vcp289 <meesompop@gmail.com>
2026-09-24 09:53:04 +0700

    Refresh cache and version for profile deletion sync (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD="81604fastfinal246";
+const BUILD="81604fastfinal247";
 const CACHE_PREFIX="lucky-number-shell-";
 const CACHE=`${CACHE_PREFIX}${BUILD}`;
 const RELEASE_BUILD="81604fastfinal13";
