6c89c1eb9905fa99482239a1966292ef433e44b0
Vcp289 <meesompop@gmail.com>
2026-09-24 12:47:43 +0700

    diagnose: identify stalled local Cloud read step safely

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD="81604fastfinal250";
+const BUILD="81604fastfinal251";
 const CACHE_PREFIX="lucky-number-shell-";
 const CACHE=`${CACHE_PREFIX}${BUILD}`;
 const RELEASE_BUILD="81604fastfinal13";
